034b000649cbc919c1b3dca7ec77c4e9aacded6a
E5Anant <e5anant2011@gmail.com>
2024-08-17 15:51:03 +0530

    Update temp.py

--- Arche-ai/temp.py
+++ Arche-ai/temp.py
@@ -1,5 +1,5 @@
-from llms import Cohere
-from agents import Agent
+from llms import Gemini
+from agents import Agent, AgentNetwork
 from tools import OwnTool, get_current_time, web_search
 
 def gcd(a:int, b:int):
@@ -41,21 +41,34 @@ time_tool = OwnTool(
 
 # Initialize the language model instance
 
-llm_instance = Cohere()
+llm_instance = Gemini()
 
-Chatbot = Agent(
-        llm=llm_instance,
-        tools=[web_tool, gcd_tool, time_tool],
-        name="ChatBot",
-        description="a powerfull ai agent",
-        sample_output="",
-        task="",
-        verbose=True,
-    )
+Chatbot1 = Agent(
+    llm=llm_instance,
+    tools=[web_tool],
+    name="Web Researcher",
+    description="An agent that specializes in web searches.",
+    sample_output="",
+    task="",
+    verbose=True,
+)
 
+Chatbot2 = Agent(
+    llm=llm_instance,
+    tools=[gcd_tool, time_tool],
+    name="Math and Time Expert",
+    description="An agent that can handle math and time-related queries.",
+    sample_output="",
+    task="",
+    verbose=True,
+)
+
+agent_network = AgentNetwork(agents=[Chatbot1, Chatbot2], tasks = [
+    {"agent": "Chatbot1", "request": "price of bitcoin??"},
+    {"agent": "Chatbot2", "request": "what is the time now??"},
+],
+ verbose=False)
 
 while True:
-    # Create the agent with multiple tools
-    Chatbot.task = input(">>>")
-    Chatbot.run()
-    # print(result)
+    response = agent_network.run()
+    print(response)
